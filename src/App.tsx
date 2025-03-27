dd4654b5d467899b9cc276d934a0dd9291dde5c6
saytowork <90796638+saytowork@users.noreply.github.com>
2025-03-27 22:47:35 +0530

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<LandingPage />} /> */}
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route element={<PrivateRoute />}>
